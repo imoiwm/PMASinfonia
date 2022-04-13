d68ba829c258be87448310cd51296c4fbcced731
jtm13 <74789040+jtm13@users.noreply.github.com>
2022-04-13 18:27:06 -0400

    Made some progress in contact page

--- contact.php
+++ contact.php
@@ -3,35 +3,76 @@
 <html>
     <head>
         <title>Contact Us</title>
-        <?php headInfo("Contacts", "Contacts, Contact Us", []); ?>
+        <?php headInfo("Contacts", "Contacts, Contact Us", ["contact"]); ?>
+        <script>
+            function mail(email, message, name) {
+                let str = "Name: " + name + "%0D%0AEmail: " + email + "%0D%0AMessage: " + message;
+                let subj = "Contact Message";
+                window.open('mailto:ugasinfonia@gmail.com?subject=' + subj + '&body=' + str);
+            }
+
+            function compose() {
+                let data = new FormData(document.getElementById("contact-form"));
+                let email = data.get('email');
+                let name = data.get('name');
+                let message = data.get('message');
+                mail(email, message, name);
+            }
+        </script>
     </head>
     <body>
         <?php head(); ?>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
+        <div id="pane">
+            <section id="right-side">
+                <form id="contact-form" action="javascript:compose()">
+                    <input id="name" name="name" type="text" placeholder="Your Name" autofocus required/>
+                    <br/>
+                    <label for="email"></label>
+                    <input id="email" name="email" type="email" placeholder="Your Email" required/>
+                    <br/>
+                    <label for="message"></label>
+                    <textarea id="message" name="message" placeholder="Your Message" maxlength="2000" required></textarea>
+                    <br/>
+                    <input class="change-button" type="submit" value="Send" />
+                </form>
+            </section>
+            <section id="left-side">
+                <table>
+                    <tr>
+                        <td class="right-col">
+                            <img class="svg-img" src="img/location.svg"></img>
+                        </td>
+                        <td class="left-col">
+                            <p class="middle-text">
+                                250 River Road<br/>
+                                Athens, GA 30602<br/>
+                                United States
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="right-col">
+                            <img class="svg-img" src="img/call.svg"></img>
+                        </td>
+                        <td class="left-col">
+                            <p class="middle-text">
+                                770-601-4123
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="right-col">
+                            <img class="svg-img" src="img/mail.svg"></img>
+                        </td>
+                        <td class="left-col">
+                            <a href="mailto:ugasinfonia@gmail.com" class="middle-text">
+                                ugasinfonia@gmail.com
+                            </a>
+                        </td>
+                    </tr>
+                </table>
+            </section>
+        </div>
         <?php foot(); ?>
     </body>
 </html>
