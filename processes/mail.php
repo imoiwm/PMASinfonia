b3d546019ed32f19eff7bfdce84d6261c5957f79
jtm13 <74789040+jtm13@users.noreply.github.com>
2022-05-12 21:32:26 -0400

    Made the emailing process better (less likely to not work)

--- processes/mail.php
+++ processes/mail.php
@@ -7,10 +7,14 @@ use PHPMailer\PHPMailer\PHPMailer;
 require("../vendor/autoload.php");
 
 $mail = new PHPMailer(true); // enable exceptions
-$mail->Host = "ssl://smtp.gmail.com:465"; // set the host (for gmail addresses)
+$mail->isSMTP(); // use an smtp server
+$mail->SMTPAuth = true; // set authentication to true
+$mail->SMTPSecure = "tls"; // gmail requires tls encryption
+$mail->Host = "smtp.gmail.com"; // set the host (for gmail addresses)
+$mail->Port = 587; // use this port
 $mail->Username = ADDRESS; // set the email username
 $mail->Password = PASSWORD; // set the email pasword
-$mail->SetFrom(ADDRESS, NAME, 0); //Set the address and name
-$mail->addReplyTo(ADDRESS, "Reply"); //Set the reply address
+$mail->SetFrom(ADDRESS, NAME); //Set the address and name
+$mail->addReplyTo(ADDRESS, "Reply", 0); //Set the reply address
 $mail->isHTML(true); //Set body to HTML
 ?>
